1a8a6f21574ba0b480e00bc976c95f292c267ec5
Tushar Parwani <167443525+Tushar-9-9@users.noreply.github.com>
2024-09-03 23:42:50 +0530

    Minor changes for deployment

--- src/index.js
+++ src/index.js
@@ -11,16 +11,13 @@ dotenv.config();
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    console.log('MongoDB connected successfully');
-})
-.catch(err => {
-    console.error('MongoDB connection error:', err);
-});
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('MongoDB connected successfully');
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
 
 // Middleware to parse JSON
 app.use(express.json());
